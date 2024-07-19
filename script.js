9696eb70671d8edad63cc67d98ff55deb64ce418
Emilie De Duyver <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-07-19 16:23:11 +0200

    tooltip adding

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/******BOOTSRAP TOOLTIP******/
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
-
 /******DROPDOWN MENU*******/
 let menuBtn = document.querySelector("#menuBtn");
 let unrolledMenuContainer = document.querySelector(".unrolledMenuContainer");
