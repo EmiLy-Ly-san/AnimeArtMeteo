8cfa7b9bcb695b96177d446b2583fb825d250be2
Emilie De Duyver <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-07-22 15:58:35 +0200

    City card interactivity

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ let menuBtn = document.querySelector("#menuBtn");
 let unrolledMenuContainer = document.querySelector(".unrolledMenuContainer");
 
 function unrollMenu() {
-  unrolledMenuContainer.classList.toggle("visible");
+  unrolledMenuContainer.classList.toggle("hidden");
 }
 
 menuBtn.addEventListener("click", unrollMenu);
+
+/*******CITY CARD ENLARGED AND REDUCED******/
+let reduceBtn = document.querySelector(".reduceBtn");
+let reducedCityCard = document.querySelector(".reducedCityCard");
+let cityCard = document.querySelector("#cityCardModal");
+let visibilityBtn = document.querySelector(".visibilityBtn");
+let citiesBtn = document.querySelectorAll(".cityBtn");
+
+function reduceEnlargeCityCard() {
+  reducedCityCard.classList.toggle("hidden");
+}
+
+function closeReducedCityCard() {
+  citiesBtn.forEach(function (cityBtn) {
+    cityBtn.addEventListener("click", () => {
+      reducedCityCard.classList.add("hidden");
+    });
+  });
+}
+
+reduceBtn.addEventListener("click", reduceEnlargeCityCard);
+
+closeReducedCityCard();
